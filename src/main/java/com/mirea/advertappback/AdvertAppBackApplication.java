d3db65111b6012cc207e595807dc7a87bbdb4278
Dmitry Vert <dmitry.vert@mail.ru>
2023-03-29 22:48:03 +0300

    init commit

---
+++ src/main/java/com/mirea/advertappback/AdvertAppBackApplication.java
@@ -0,0 +1,12 @@
+package com.mirea.advertappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertAppBackApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mirea/advertappback/AdvertAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirea.advertappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
