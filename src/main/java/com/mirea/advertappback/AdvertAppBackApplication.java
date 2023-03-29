218d950f7496226e910da7998da10761efc2da37
Dmitry Vert <dmitry.vert@mail.ru>
2023-03-29 23:04:21 +0300

    health check commit

--- src/main/java/com/mirea/advertappback/AdvertAppBackApplication.java
+++ src/main/java/com/mirea/advertappback/AdvertAppBackApplication.java
@@ -2,8 +2,9 @@ package com.mirea.advertappback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AdvertAppBackApplication {
 
 	public static void main(String[] args) {
